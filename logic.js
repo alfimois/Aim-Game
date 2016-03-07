44ca8b8266fc91a70fa82bb592a2946741eca7f9
alfimois <alfimois@gmail.com>
2016-03-08 02:03:46 +0300

    hello world

---
+++ logic.js
@@ -0,0 +1,31 @@
+$( document ).ready( function () 
+{
+    var game  = $("#game"),
+        score = $("#score"),
+        countScore = 0;
+    function random( min, max )
+    {
+      return Math.floor( Math.random() * (max - min + 1) ) + min;
+    }
+    game.on("click", function (e)
+    {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    });
+
+	function newTarget() {
+        var target = $( "<div></div>", { class : "target" } ),
+            left   = random( 0, game.width() - target.width() ),
+            top    = random( 0, game.height() - target.height() );
+
+        $( target ).appendTo( game ).offset( { left : left, top : top } ).on("mousedown", function  () 
+        {
+            target.addClass("remove");
+            target.remove();
+            score.text( ++countScore );
+        });
+
+    };
+    setInterval( newTarget, 4000 );
+
+} );
