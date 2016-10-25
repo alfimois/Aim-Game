07cb1113ebf301ec9ab263e6c88d976a3162baba
alfimois <alfimois@gmail.com>
2016-10-25 17:10:25 +0300

    Сделал код более читабельным и саму игру более легче

--- logic.js
+++ logic.js
@@ -1,60 +1,38 @@
-$( document ).ready( function () 
-{
-    var time = +prompt("Delay in ms?") || 700;
+$(document).ready(function() {
+    var time       = +prompt("Delay in ms?") || 700;
+    var game       = $("#game");
+    var score      = $("#score");
+    var countScore = 0;
 
-    var game  = $("#game"),
-        score = $("#score"),
-        countScore = 0;
-    console.log( game.width() );
-    
-    function random( min, max )
-    {
+
+    function random(min, max){
         return Math.floor( Math.random() * (max - min + 1) ) + min;
-    }
-
-    game.on("click", function (e)
-    {   
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        game.addClass("cursorView");
-        setTimeout(function ()
-        {
-            game.removeClass("cursorView");
-        }, 70);
-    });
+    };
 
 
-	function newTarget() 
-    {
-        
-        var target = $( "<div></div>", { class : "target" } );
+	function newTarget() {
+        var target = $("<div></div>", { class : "target" });
+        var left   = random(0, game.width()  - target.width());
+        var top    = random(0, game.height() - target.height() - 20);
 
-        $( target ).appendTo( game );
-
-
-        var left   = random( 0, game.width() - target.width() ),
-            top    = random( 0, game.height() - target.height() - 20 );
+        $(target).appendTo(game);
 
         target.offset({ left : left, top : top })
-        .animate({transform : "scale(0.1)"}, 
+        .animate({transform : "scale(0.1)"},
         {
-          duration: 6000,
-          easing: "linear",
-          complete: function () 
-          {
+            duration: 6000,
+            easing  : "linear",
+            complete: function () {
+                target.addClass("remove");
+                target.remove();
+            }
+        })
+        .on("mousedown", function  () {
             target.addClass("remove");
             target.remove();
-        } 
-    })
-        .on("mousedown", function  () 
-        {
-            target.addClass("remove");
-            target.remove();
-            score.text( ++countScore );
+            score.text(++countScore);
         });
-
-
     };
-    setInterval( newTarget, time );
+    setInterval(newTarget, time);
 
-} );
+});
