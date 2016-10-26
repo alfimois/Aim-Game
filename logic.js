ab085dc48c4bc5bd3e9853e3a0fee96d8e80faa7
alfimois <alfimois@gmail.com>
2016-10-26 15:17:48 +0300

    update logic.js

--- logic.js
+++ logic.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
-    var time       = +prompt("Delay in ms?") || 700;
-    var game       = $("#game");
-    var score      = $("#score");
-    var countScore = 0;
+$(document).ready( function() {
+  var time       = Number( prompt("Delay in ms?") ) || 700;
+  var game       = $("#game");
+  var score      = $("#score");
+  var countScore = 0;
 
 
-    function random(min, max){
-        return Math.floor( Math.random() * (max - min + 1) ) + min;
-    };
+  function random(min, max) {
+    return Math.floor( Math.random() * (max - min + 1) ) + min;
+  };
 
 
 	function newTarget() {
-        var target = $("<div></div>", { class : "target" });
-        var left   = random(0, game.width()  - target.width());
-        var top    = random(0, game.height() - target.height() - 20);
+    var target = $("<div></div>", { class : "target" });
+    var left   = random(0, game.width()  - target.width()  - 65);
+    var top    = random(0, game.height() - target.height() - 65);
 
-        $(target).appendTo(game);
+    $(target).appendTo(game);
 
-        target.offset({ left : left, top : top })
-        .animate({transform : "scale(0.1)"},
-        {
-            duration: 6000,
-            easing  : "linear",
-            complete: function () {
-                target.addClass("remove");
-                target.remove();
-            }
-        })
-        .on("mousedown", function  () {
-            target.addClass("remove");
-            target.remove();
-            score.text(++countScore);
-        });
-    };
-    setInterval(newTarget, time);
+    target.offset({ left : left, top : top })
+      .animate({transform : "scale(0.1)"}, {
+        duration: 6000,
+        easing  : "linear",
+        complete: function () {
+          target.addClass("remove");
+          target.remove();
+        }
+      })
+      .on("mousedown", function  () {
+        target.addClass("remove");
+        target.remove();
+        score.text(++countScore);
+      });
+  };
+
+  setInterval(newTarget, time);
 
 });
