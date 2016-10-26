b0babc7238613cde883374e4c3cc9fd6c38d99dd
alfimois <alfimois@gmail.com>
2016-10-26 15:52:31 +0300

    es2015 update

--- logic.js
+++ logic.js
@@ -1,33 +1,31 @@
 $(document).ready( function() {
-  var time       = Number( prompt("Delay in ms?") ) || 700;
-  var game       = $("#game");
-  var score      = $("#score");
-  var countScore = 0;
+  const timeOfLifeTarget = Number( prompt("Напишите время появления новой мишени (в мс)") ) || 700;
+  let gameWindow         = $(".gameWindow");
+  let scoreWindow        = $(".scoreWindow");
+  let countScore         = 0;
 
   function random(min, max) {
     return Math.floor( Math.random() * (max - min + 1) ) + min;
   };
 
   function newTarget() {
-    var target = $("<div></div>", { class : "target" });
-    var left   = random(0, game.width()  - target.width()  - 65);
-    var top    = random(0, game.height() - target.height() - 65);
+    let target = $("<div></div>", { class : "target" });
+    let left   = random(0, gameWindow.width()  - target.width()  - 65);
+    let top    = random(0, gameWindow.height() - target.height() - 65);
 
-    $(target).appendTo(game);
+    $(target).appendTo(gameWindow);
 
     target.offset({ left : left, top : top })
       .animate({transform : "scale(0.1)"}, {
         duration: 4000,
         easing  : "linear",
-        complete: function () {
-          target.remove();
-        }
+        complete: () => target.remove()
       })
-      .on("mousedown", function  () {
+      .on("mousedown", () => {
         target.remove();
-        score.text(++countScore);
+        scoreWindow.text(++countScore);
       });
   };
 
-  setInterval(newTarget, time);
+  setInterval(newTarget, timeOfLifeTarget);
 });
