32bb072da117c0b9df7011385a3cea3531149b3a
Yarik Leto <alfimois@gmail.com>
2018-03-28 21:19:44 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,30 +1,30 @@
-$(document).ready( function() {
+$(document).ready(() => {
   const timeOfLifeTarget = Number(
     prompt(
-      "Введите время в миллисекундах.\n \
-       1000 миллисекунд равняется 1 секунде.\n \
-       Стандартное значение равняется 1 секунде.\n \
-       Чтобы включить режим на весь экран: нажмите F12"
+      `Введите время в миллисекундах.\n
+       1000 миллисекунд равняется 1 секунде.\n
+       Стандартное значение равняется 1 секунде.\n
+       Чтобы включить режим на весь экран: нажмите F12`
     )
-  ) || 1000;
+  ) || 1e3;
   const gameWindow       = $(".gameWindow");
   const scoreWindow      = $(".scoreWindow");
   let countScore         = 0;
 
-  function random(min, max) {
+  const random = (min, max) => {
     return Math.floor( Math.random() * (max - min + 1) ) + min;
   };
 
-  function newTarget() {
-    let target = $("<div></div>", { class : "target" });
-    let left   = random(0, gameWindow.width()  - target.width()  - 65);
-    let top    = random(0, gameWindow.height() - target.height() - 65);
+  const newTarget => {
+    const target = $("<div></div>", { class : "target" });
+    const left   = random(0, gameWindow.width()  - target.width()  - 65);
+    const top    = random(0, gameWindow.height() - target.height() - 65);
 
     $(target).appendTo(gameWindow);
 
-    target.offset({ left : left, top : top })
+    target.offset({ left, top })
       .animate({transform : "scale(0.1)"}, {
-        duration: 4000,
+        duration: 4e3,
         easing  : "linear",
         complete: () => target.remove()
       })
